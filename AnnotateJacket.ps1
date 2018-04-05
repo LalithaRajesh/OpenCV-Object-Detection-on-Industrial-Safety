0fb6ee3ca026ddde6d5c34d94609800be4559459
LalithaRajesh <36064287+LalithaRajesh@users.noreply.github.com>
2018-04-05 14:48:21 +0530

    Annotate files for Jacket

---
+++ AnnotateJacket.ps1
@@ -0,0 +1,6 @@
+﻿$datafile = 'info/info_AnnotateJacketRS.data'
+$opencv_annotations = 'C:\Users\lalit\Miniconda3\Library\bin\opencv_annotation.exe'
+$folderpath = '.\Images\Safety_Jacket\Jacket_Resized'
+
+
+& $opencv_annotations --annotations=$datafile --images=$folderpath
