ebc45f54fe24e3947787db44ad016d49dfa87a73
LalithaRajesh <36064287+LalithaRajesh@users.noreply.github.com>
2018-04-05 16:08:08 +0530

    Update AnnotateHelmet.ps1

--- AnnotateHelmet.ps1
+++ AnnotateHelmet.ps1
@@ -1,4 +1,4 @@
-﻿$datafile = '.\info\info_AnnotateHelmetRS.data'
+﻿$datafile = 'info_AnnotateHelmetRS.data'
 $opencv_annotations = 'C:\Users\lalit\Miniconda3\Library\bin\opencv_annotation.exe'
 $folderpath = '.\Images\Safety_Helmet\helmet_resized'
 
