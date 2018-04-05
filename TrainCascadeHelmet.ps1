e8f118c713921c0f35c02d75ede0629e10c8d24f
LalithaRajesh <36064287+LalithaRajesh@users.noreply.github.com>
2018-04-05 15:00:03 +0530

    Train Cascade

---
+++ TrainCascadeHelmet.ps1
@@ -0,0 +1,4 @@
+﻿$opencv_traincascade = 'C:\Users\lalit\Miniconda3\Library\bin\opencv_traincascade.exe'
+
+& $opencv_traincascade -data Helmetcascade -vec PosHelmet.vec -bg bg.txt -numPos 20 -numNeg 2000 -numStages 10 -w 50 -h 50
+  
