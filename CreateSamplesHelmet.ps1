0f86a7337d618c5469d17fddd1676daf1077ba29
LalithaRajesh <36064287+LalithaRajesh@users.noreply.github.com>
2018-04-05 14:49:12 +0530

    Create Samples.ps1

---
+++ CreateSamplesHelmet.ps1
@@ -0,0 +1,6 @@
+﻿$infofile = 'info_AnnotateHelmetRS.data'
+$opencv_createsamples = 'C:\Users\lalit\Miniconda3\Library\bin\opencv_createsamples.exe'
+$numpics = 20
+
+
+& $opencv_createsamples -info $infofile -num $numpics -vec PosHelmet.vec -maxxangle 0.6 -maxyangle 0 -maxzangle 0.3 -maxidev 60 -bgcolor 0 -bgthresh 0 -w 50 -h 50    
